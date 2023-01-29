4df935d3d96228223d8f3215495627526164dcc7
Damilola <Owner@DESKTOP-UFU8DHC>
2023-01-29 15:57:11 -0500

    ticket number

--- jenkinsscript.sh
+++ jenkinsscript.sh
@@ -13,12 +13,12 @@ apt-get install docker-ce docker-ce-cli containerd.io
 systemctl enable docker
 systemctl start docker
 usermod -aG docker ubuntu
-
+## apt-get install vim -y
 # run jenkins
 mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
 docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
-
+## extra packages
 # show endpoint
 echo 'Jenkins installed'
 echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
